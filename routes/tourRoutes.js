401224d8bebbf542b9b6f97e64358aa5c9f36ae3
DIKI YUDA <dickyudhz@gmail.com>
2023-09-25 23:36:54 +0700

    first commit

---
+++ routes/tourRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const toursController = require("../controllers/toursController")
+
+const router = express.Router()
+
+// router.param("id", toursController.checkId)
+
+router
+  .route("/")
+  .get(toursController.getAllTours)
+  .post(
+    toursController.checkBody,
+    toursController.createTour
+  )
+
+router
+  .route("/model")
+  .post(toursController.createTourModel)
+
+router
+  .route("/:id")
+  .get(toursController.getTourById)
+  .patch(toursController.editTour)
+  .delete(toursController.removeTour)
+
+module.exports = router
